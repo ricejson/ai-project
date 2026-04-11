93463f8b91fe12d90334f458f5d56af2673d3315
ricejson <wjjbangbangbang@163.com>
2026-04-11 21:41:21 +0800

    or运算符重写

--- rag/11-【扩展】or运算符重写.py
+++ rag/11-【扩展】or运算符重写.py
@@ -1,14 +1,22 @@
 class Test(object):
     def __init__(self, name):
         self.name = name
-        self.members = [self]
+
+    def __or__(self, other):
+        return MySequence(self, other)
+
+    def __str__(self):
+        return self.name
+
+class MySequence(object):
+    def __init__(self, *args):
+        self.members = []
+        for member in args:
+            self.members.append(member)
 
     def __or__(self, other):
         self.members.append(other)
         return self
-
-    def __str__(self):
-        return self.name
 
     def invoke(self):
         for member in self.members:
